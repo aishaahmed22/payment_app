a9a8c2b8091b27b206dc7cc00cfe90d7f67591db
aishaa2022 <120982674+aishaa2022@users.noreply.github.com>
2024-04-28 13:51:10 +0300

    ...

--- lib/features/checkout/presentation/views/widgets/payment_method_item.dart
+++ lib/features/checkout/presentation/views/widgets/payment_method_item.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class PaymentMethodItem extends StatelessWidget {
   const PaymentMethodItem(
-      {super.key, this.isActive = false, required this.image});
+      {super.key, required this.isActive, required this.image});
   final bool isActive;
   final String image;
   @override
@@ -27,17 +26,22 @@ class PaymentMethodItem extends StatelessWidget {
                 spreadRadius: 0)
           ]),
       child: Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: SvgPicture.asset(
-          image,
-          height: 24,
-          fit: BoxFit.scaleDown,
-        ),
-      ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Image.asset(
+            image,
+            height: 24,
+            fit: BoxFit.scaleDown,
+          )
+          // SvgPicture.asset(
+          //   image,
+          //   height: 24,
+          //   fit: BoxFit.scaleDown,
+          // ),
+          ),
     );
   }
 }
--- lib/features/checkout/presentation/views/widgets/payment_methods_bottom_sheet.dart
+++ lib/features/checkout/presentation/views/widgets/payment_methods_bottom_sheet.dart
@@ -1,24 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:payment_app/core/widgets/payment_method_list_view.dart';
-import 'package:payment_app/features/checkout/presentation/views/widgets/custom_botton_bloc_consumer.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:payment_app/features/checkout/presentation/cubit/paymob_cubit/paymob_cubit.dart';
+import 'package:payment_app/features/checkout/presentation/cubit/paymob_cubit/paymob_states.dart';
+import 'package:payment_app/features/checkout/presentation/cubit/stripe_cubit/stripe_cubit.dart';
+import 'package:payment_app/features/checkout/presentation/views/widgets/payment_method_item.dart';
+import 'package:payment_app/features/data/models/payment_intent_input_model/payment_intent_input_model.dart';
 
-class PaymentButtonSheet extends StatelessWidget {
+class PaymentButtonSheet extends StatefulWidget {
   const PaymentButtonSheet({super.key});
 
   @override
+  State<PaymentButtonSheet> createState() => _PaymentButtonSheetState();
+}
+
+class _PaymentButtonSheetState extends State<PaymentButtonSheet> {
+  int indexColor = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          PaymentMethodListView(),
-          SizedBox(
-            height: 16,
-          ),
-          CustomBottonBlocConsumer(),
-        ],
-      ),
-    );
+    return Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          children: [
+            BlocConsumer<StripePaymentCubit, StripePaymentState>(
+              listener: (context, state) {
+                if (state is StripePaymentSucess) {
+                  // Navigator.of(context).pushReplacement(
+                  //   MaterialPageRoute(
+                  //     builder: (context) => const ThankYouView(),
+                  //   ),
+                  // );
+                }
+                if (state is StripePaymentFailur) {
+                  Navigator.pop(context);
+                  SnackBar snackBar = SnackBar(content: Text(state.error));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+                if (state is StripePaymentLoading) {
+                  const CircularProgressIndicator();
+                }
+              },
+              builder: (state, index) => GestureDetector(
+                  onTap: () {
+                    indexColor = 0;
+
+                    setState(() {});
+                    PaymentIntentInputModel paymentIntentInputModel =
+                        PaymentIntentInputModel(
+                            amount: '100',
+                            currency: "USD",
+                            customerId: "cus_PzKSUUPBfL6RTT");
+                    BlocProvider.of<StripePaymentCubit>(context).makePayment(
+                        paymentIntentInputModel: paymentIntentInputModel);
+                  },
+                  child: PaymentMethodItem(
+                    image:
+                        "assets/png-clipart-credit-card-debit-card-mastercard-payment-card-credit-card-payment-internet.png",
+                    isActive: indexColor == 0,
+                  )),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            BlocConsumer<PaymobPaymentCubit, PaymobPaymentState>(
+                listener: (context, state) {
+              if (state is PaymobPaymentSucess) {}
+              if (state is PaymobPaymentFailur) {
+                Navigator.pop(context);
+                SnackBar snackBar = SnackBar(content: Text(state.error));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }
+              if (state is PaymobPaymentLoading) {
+                const CircularProgressIndicator();
+              }
+            }, builder: (context, state) {
+              return GestureDetector(
+                  onTap: () {
+                    indexColor = 1;
+                    setState(() {});
+                    BlocProvider.of<PaymobPaymentCubit>(context)
+                        .makePayment(amount: 10, currency: "EGP");
+                  },
+                  child: PaymentMethodItem(
+                    isActive: indexColor == 1,
+                    image: "assets/PayMob_Payments.png",
+                  ));
+            })
+          ],
+        ));
   }
 }
